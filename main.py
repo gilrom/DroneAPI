3c272e70312900c88c088c7d5978640265a37960
Gil Romano <gilrom@gmail.com>
2023-03-01 18:29:17 +0200

    FIrst commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from DroneClient import DroneClient
+import time
+import airsim.utils
+
+if __name__ == "__main__":
+    client = DroneClient()
+    client.connect()
+
+    print(client.isConnected())
+
+    time.sleep(4)
+    client.setAtPosition(-346, -700, -100)
+
+    # time.sleep(3)
+    # client.flyToPosition(-346, -420, -100, 10)
+
+    while True:
+        print(client.getLidarData())
+        time.sleep(1)
+
+        
