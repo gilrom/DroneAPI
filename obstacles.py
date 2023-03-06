172c377f7f81206b06d8cccb535dcad285691c39
Gil Romano <gilrom@gmail.com>
2023-03-06 11:24:36 +0200

    box obstacles

--- obstacles.py
+++ obstacles.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from shapely.geometry import Polygon,Point,box,LineString
 import pandas as pd
+import numpy as np
 
 obs = pd.read_csv("obstacles_100m_above_sea_level.csv")
 types = obs["obs type"].unique()
@@ -10,11 +11,11 @@ for type in types:
     print(df)
     coords = []
     for index, row in df.iterrows():
-        print(row)
         coord = (row["x obs meter"], row["y obs meter"])
         coords.append(coord)
-        # print(coord)
-    polygons.append(Polygon(coords))
+    x_coords = [x for (x,y)in coords]
+    y_coords = [y for (x,y)in coords]
+    polygons.append(box(np.min(x_coords), np.min(y_coords), np.max(x_coords), np.max(y_coords)))
 
 fig = plt.figure()
 plot = plt.subplot(1,1,1)
